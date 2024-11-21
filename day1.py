1f1e67be9a9030996553c9f37cc5404b562f88b4
Nishchit Bhandari <nishchitbhandari2061@gmail.com>
2024-11-21 11:05:22 +0545

    Add day 1 codes

---
+++ day1.py
@@ -0,0 +1,39 @@
+print("Hello world")  # Code to print hello world
+"""
+This line prints hello world on terminal
+Syntax: print(<variable/data>)
+"""
+print(1)  # This is integer data type
+print(1.2)  # This is float data type
+print([1, 2, 3, 4, 5, "Nishchit"])  # This is list data type
+
+
+# Variable: A location on computer memory that stores data.
+name = "Nishchit"  # name variable stores "Nishchit" string
+python1 = "3.12.7"  # python1 variable stores "3.12.7" string
+python2 = "3.13"
+num1 = 53  # num1 variable stores integer 5
+num2 = 6  # num2 variable stores integer 6
+
+addition = num1 + num2  # Addition is done using + operator
+print("The result of addition is", addition, "subtraction")
+subtraction = num1 - num2  # Subtraction is done using - operator
+multiplication = num1 * num2  # Multiplication is done using * operator
+division = num1 / num2  # Division is done using / operator
+integral_division = num1 // num2  # Integral division is done using // operator
+modular_div = num1 % num2 # Modular division is done using % operator
+print(
+    "The result of subtraction is " + str(subtraction)
+)  # Concatenation: Adding two strings
+power = num1**num2  # Power operation is done using ** operator
+
+# f-strings
+print(f"The result of multiplication is {multiplication}, subtraction is {subtraction}")
+print(
+    f"The result of division is {division} and the result of integral division is {integral_division}"
+)
+print(f"{num1} to the power {num2} is {power}")
+print(f"The result of modular division is {modular_div}")
+
+fruits = ['apples', 'bananas', 'grapes', 'strawberries', 'mangoes', 'pineapples', 'a']
+new_list = fruits[2:]
